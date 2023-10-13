d46696780a0c1ca99e0f8809a2c8bdb4091f435a
TheGhossst <nandukrishnapkm97@gmail.com>
2023-10-13 18:42:22 +0530

    add: constructor det

--- src/hangman.py
+++ src/hangman.py
@@ -3,6 +3,12 @@ from random_word import RandomWords
 
 class HangmanGame:
     def __init__(self, root):
+        self.root = root
+        self.r = RandomWords()
+        self.word = self.r.get_random_word().upper()
+        self.guesses_left = 6
+        self.display_word = ["_"] * len(self.word)
+        self.previous_guesses = []
 def main():
     root = tk.Tk()
     root.title("Hangman Game")
